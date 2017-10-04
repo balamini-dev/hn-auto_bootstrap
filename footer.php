9059d2a4f68dfd10b5befb10792e18d85862d08c
balamini-dev <b.diaby@hotmail.fr>
2017-10-04 03:41:20 +0200

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
 <footer>
 	<div class="l1">
-		<img class="image_footer" src="email_icon.png" alt="">
-		<p class="text_footer"><a href ="mailto:b.diaby@hotmail.fr" class="text_footer" title="envoyer un mail" target="_blank">b.diaby@hotmail.fr</p>
+		<img class="image_footer" src="envelope.png" alt="">
+		<p class="text_footer">hnauto@hn.fr</p>
 	</div>
 
 	<div class="l1">
-		<img class="image_footer" src="github_icon.png" alt="">
-		<p class="text_footer"><a href="https://github.com/balamini-dev" class="text_footer" title="cliquer pour accéder au github" target="_blank">balamini-dev</a></p>
+		<img class="image_footer" src="telephone.png" alt="">
+		<p class="text_footer">06 00 00 00 00</p>
 	</div>
 
 	<div class="l1">
-		<p class="text_footer">réalisé par Balamini Diaby</p>
+		<p class="text_footer">réalisé par Balamini Diaby et Bakary Coly</p>
 	</div>
 </div>
 
